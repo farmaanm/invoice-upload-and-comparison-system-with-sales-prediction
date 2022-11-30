f24fd06d75743d0f900df215df07722a43944b15
Farmaan <mfmohamed@students.nsbm.ac.lk>
2022-11-30 11:38:46 +0530

    Initialized React Application

---
+++ Web/invois/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/login';
+import File from './components/file';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <File />
+    </div>
+  );
+}
+
+export default App;
