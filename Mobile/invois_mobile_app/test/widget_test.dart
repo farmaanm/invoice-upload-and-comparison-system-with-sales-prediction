238d11f2a33cdeed3a66e35c76a25c4787f02d89
Farmaan <77222043+farmaanm@users.noreply.github.com>
2023-03-28 12:16:52 +0530

    Update widget_test.dart

--- Mobile/invois_mobile_app/test/widget_test.dart
+++ Mobile/invois_mobile_app/test/widget_test.dart
@@ -1,30 +1,38 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:integration_test/integration_test.dart';
 import 'package:invois_mobile_app/main.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+void main() async {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  group('Splash Screen', () {
+    testWidgets('Image Asset', (WidgetTester tester) async {
+      
+      const widget = SplashScreen();
+      final element = widget.createElement(); // this will set state.widget
+      final state = element.state as SplashScreenState;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      expect(state.methodToBeTested(), 0);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });*/
+    });
+  });
+
+  group('Login Screen', () {
+
+    testWidgets('Login Credentials error', (WidgetTester tester) async {
+
+      const widget = MyLoginPage(title: '');
+      final element = widget.createElement();
+      final state = element.state as MyLoginPageState;
+
+      expect(state.useremailerror, '');
+      expect(state.userpassworderror, '');
+
+    });
+
+  });
+
 }
+
+//flutter test test/widget_test.dart
